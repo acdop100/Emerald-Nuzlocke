73a58eecfb5a83b5492d1c0b698b5814d3fa6b4d
Alex <j.alexander.lehman@gmail.com>
2019-12-17 17:03:35 -0500

    Updated to current branch

--- include/clear_save_data_menu.h
+++
@@ -1,6 +0,0 @@
-#ifndef GUARD_CLEAR_SAVE_DATA_MENU_H
-#define GUARD_CLEAR_SAVE_DATA_MENU_H
-
-void CB2_InitClearSaveDataScreen(void);
-void Task_ClearSaveData2();
-#endif // GUARD_CLEAR_SAVE_DATA_MENU_H
--- src/librfu_intr.c
+++
@@ -1,5 +0,0 @@
-#include "global.h"
-#include "main.h"
-#include "librfu.h"
-
-//TODO: decompile asm/librfu_intr.s to here
